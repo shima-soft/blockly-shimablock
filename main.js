0955fe68e0e663d6c6bf7e82177efc283b0958d0
shima-soft <yshimabukuro@gmail.com>
2021-06-06 17:08:03 +0900

    ワークスペース上部ボタン用スクリプトファイルを追加

---
+++ main.js
@@ -0,0 +1,56 @@
+function setStartBlock() {
+  workspace.clear();
+  let xml = '<xml xmlns="https://developers.google.com/blockly/xml"><block type="excel_start" editable="false" deletable="false" movable="false" x="30" y="30"></block></xml>';
+  let dom = Blockly.Xml.textToDom(xml);
+  Blockly.Xml.domToWorkspace(dom, workspace);
+}
+
+function setProcedureName() {
+  Blockly.VBA.PROCEDURE_NAME = document.getElementById("project_name").value;
+}
+
+function readXmlToWorkspace(input) {
+  let file = input.files[0];
+  let reader = new FileReader();
+  reader.readAsText(file);
+
+  reader.onload = function() {
+    var xml = reader.result
+    xml = Blockly.Xml.textToDom(xml);
+    workspace.clear();
+    Blockly.Xml.domToWorkspace(xml, workspace);
+
+    let project_name = file.name;
+    project_name = project_name.replace(".xml","");
+    project_name = project_name.replace("shimablock_","");
+    project_name = project_name.replace("(","");
+    project_name = project_name.replace(")","");
+    project_name = project_name.replace(" ","");
+    document.getElementById("loadButton").value = "";
+
+    document.getElementById("project_name").value = project_name;
+    setProcedureName();
+  };
+
+  reader.onerror = function() {
+    alert("ファイル読み込み時にエラーが発生しました。")
+    console.log(reader.error);
+  };
+
+}
+
+function downloadXml() {
+  var project_name = document.getElementById("project_name").value;
+  var download_file_name = "shimablock_" + project_name + ".xml";
+  document.getElementById("download").download = download_file_name;
+
+  var xml = Blockly.Xml.workspaceToDom(workspace);
+  var xml_text = Blockly.Xml.domToPrettyText(xml);
+  var content = xml_text;
+  var blob = new Blob([ content ], { "type" : "text/xml" });
+  if (window.navigator.msSaveBlob) { 
+      window.navigator.msSaveOrOpenBlob(blob, download_file_name); 
+  } else {
+      document.getElementById("download").href = window.URL.createObjectURL(blob);
+  }
+}
