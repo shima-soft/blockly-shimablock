d52379c3d0d0c0b19744dcb35e0c448f37db4b32
shima-soft <yshimabukuro@gmail.com>
2022-01-13 21:24:16 +0900

    フォルダ整理

---
+++ blocks/add/py_func.js
@@ -0,0 +1,197 @@
+
+Blockly.defineBlocksWithJsonArray(
+  [{
+    "type": "py_func_open_text",
+    "message0": "テキストファイルを開く(自動クローズ) %1 ファイルパス： %2 モード： %3 %4 エンコード： %5 %6 %7",
+    "args0": [
+      {
+        "type": "input_dummy"
+      },
+      {
+        "type": "input_value",
+        "name": "PATH",
+        "check": "String",
+        "align": "RIGHT"
+      },
+      {
+        "type": "field_dropdown",
+        "name": "MODE",
+        "options": [
+          [
+            "指定なし",
+            "none"
+          ],
+          [
+            "rt 読み取り用(既存データ：影響なし、位置：先頭)(既定)",
+            "rt"
+          ],
+          [
+            "wt 書き込み用(既存データ：削除する、位置：先頭)",
+            "wt"
+          ],
+          [
+            "at 書き込み用(既存データ：削除しない、位置：末尾)",
+            "at"
+          ],
+          [
+            "xt 書き込み＆新規作成(既存データ：影響なし、位置：先頭)",
+            "xt"
+          ],
+          [
+            "r+t 読み書き用(既存データ：削除しない、位置：先頭)",
+            "r+t"
+          ],
+          [
+            "w+t 読み書き用(既存データ：削除する、位置：先頭)",
+            "w+t"
+          ],
+          [
+            "a+t 読み書き用(既存データ：削除しない、位置：末尾)",
+            "a+t"
+          ],
+          [
+            "x+t 読み書き＆新規作成(既存データ：影響なし、位置：先頭)",
+            "x+t"
+          ]
+        ]
+      },
+      {
+        "type": "input_dummy",
+        "align": "RIGHT"
+      },
+      {
+        "type": "field_dropdown",
+        "name": "ENCODING",
+        "options": [
+          [
+            "指定なし",
+            "none"
+          ],
+          [
+            "cp932",
+            "cp932"
+          ],
+          [
+            "euc_jp",
+            "euc_jp"
+          ],
+          [
+            "iso2022_jp",
+            "iso2022_jp"
+          ],
+          [
+            "shift_jis",
+            "shift_jis"
+          ],
+          [
+            "utf_8",
+            "utf_8"
+          ],
+          [
+            "utf_16",
+            "utf_16"
+          ],
+          [
+            "utf_32",
+            "utf_32"
+          ]
+        ]
+      },
+      {
+        "type": "input_dummy",
+        "align": "RIGHT"
+      },
+      {
+        "type": "input_statement",
+        "name": "DO"
+      }
+    ],
+    "inputsInline": false,
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 30,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "py_func_open_read",
+    "message0": "%1 変数に全行読み込む",
+    "args0": [
+      {
+        "type": "field_variable",
+        "name": "NAME",
+        "variable": "all_line"
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 30,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "py_func_open_readline",
+    "message0": "%1 変数に1行読み込む",
+    "args0": [
+      {
+        "type": "field_variable",
+        "name": "NAME",
+        "variable": "line"
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 30,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "py_func_open_readlines",
+    "message0": "%1 リストに全行読み込む",
+    "args0": [
+      {
+        "type": "field_variable",
+        "name": "NAME",
+        "variable": "line_list"
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 30,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "py_func_open_write",
+    "message0": "%1 文字列(変数)を書き込む",
+    "args0": [
+      {
+        "type": "input_value",
+        "name": "TEXT"
+      }
+    ],
+    "inputsInline": true,
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 30,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "py_func_open_writelines",
+    "message0": "%1 リストを書き込む",
+    "args0": [
+      {
+        "type": "field_variable",
+        "name": "LIST",
+        "variable": "line_list"
+      }
+    ],
+    "inputsInline": true,
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 30,
+    "tooltip": "",
+    "helpUrl": ""
+  }]
+  );
---
+++ blocks/add/selenium_webdriver.js
@@ -0,0 +1,890 @@
+Blockly.defineBlocksWithJsonArray(
+  [{
+    "type": "driver_var_set",
+    "message0": "%1 %2 に %3 をセット",
+    "args0": [
+      {
+        "type": "field_variable",
+        "name": "OBJECT_NAME",
+        "variable": "driver",
+        "variableTypes": ["driver"],
+        "defaultType": "driver"    
+      },
+      {
+        "type": "input_dummy"
+      },
+      {
+        "type": "field_dropdown",
+        "name": "driver_NAME",
+        "options": [
+          [
+            "Chromeドライバ",
+            "Chrome"
+          ],
+          [
+            "Edgeドライバ",
+            "Edge"
+          ],
+          [
+            "Firefoxドライバ",
+            "Firefox"
+          ],
+          [
+            "Operaドライバ",
+            "Opera"
+          ],
+          [
+            "IEドライバ",
+            "Ie"
+          ]
+        ]
+      }
+    ],
+    "inputsInline": true,
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 180,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "driver_get",
+    "message0": "%1 から %2 を取得",
+    "args0": [
+      {
+        "type": "field_variable",
+        "name": "OBJECT_NAME",
+        "variable": "driver",
+        "variableTypes": ["driver"],
+        "defaultType": "driver"
+      },
+      {
+        "type": "field_dropdown",
+        "name": "METHOD",
+        "options": [
+          [
+            "URL",
+            ".current_url"
+          ],
+          [
+            "タイトル",
+            ".title"
+          ],
+          [
+            "ページソース",
+            ".page_source"
+          ],
+          [
+            "ウィンドウ位置",
+            ".get_window_position()"
+          ],
+          [
+            "ウィンドウサイズ",
+            ".get_window_size()"
+          ],
+          [
+            "ウィンドウ位置とサイズ",
+            ".get_window_rect()"
+          ],
+          [
+            "ブラウザ名",
+            ".name"
+          ],
+          [
+            "現在のウィンドウハンドル",
+            ".current_window_handle"
+          ],
+          [
+            "全てのウィンドウハンドル",
+            ".window_handles"
+          ]
+        ]
+      }
+    ],
+    "output": [
+      "String",
+      "Array"
+    ],
+    "colour": 180,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "driver_get_cookie",
+    "message0": "%1 から名前: %2 の クッキーを配列で取得",
+    "args0": [
+      {
+        "type": "field_variable",
+        "name": "OBJECT_NAME",
+        "variable": "driver",
+        "variableTypes": ["driver"],
+        "defaultType": "driver"
+      },
+      {
+        "type": "input_value",
+        "name": "NAME",
+        "check": "String"
+      }
+    ],
+    "output": "Array",
+    "colour": 180,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "driver_get_cookies",
+    "message0": "%1 から全てのクッキーを配列で取得",
+    "args0": [
+      {
+        "type": "field_variable",
+        "name": "OBJECT_NAME",
+        "variable": "driver",
+        "variableTypes": ["driver"],
+        "defaultType": "driver"
+      }
+    ],
+    "output": "Array",
+    "colour": 180,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "driver_get_find_element_by",
+    "message0": "%1 から %2 %3 の最初の element を取得",
+    "args0": [
+      {
+        "type": "field_variable",
+        "name": "OBJECT_NAME",
+        "variable": "driver",
+        "variableTypes": ["driver"],
+        "defaultType": "driver"
+      },
+      {
+        "type": "field_dropdown",
+        "name": "METHOD",
+        "options": [
+          [
+            "ID名:",
+            "id"
+          ],
+          [
+            "name:",
+            "name"
+          ],
+          [
+            "クラス名:",
+            "class_name"
+          ],
+          [
+            "リンク名:",
+            "link_text"
+          ],
+          [
+            "一部のリンク名:",
+            "partial_link_text"
+          ],
+          [
+            "タグ名:",
+            "tag_name"
+          ],
+          [
+            "CSSセレクタ:",
+            "css_selector"
+          ],
+          [
+            "XPath:",
+            "xpath"
+          ]
+        ]
+      },
+      {
+        "type": "input_value",
+        "name": "NAME",
+        "check": "String"
+      }
+    ],
+    "inputsInline": true,
+    "output": "element",
+    "colour": 180,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "driver_get_find_elements_by",
+    "message0": "%1 から %2 %3 の全ての element を取得",
+    "args0": [
+      {
+        "type": "field_variable",
+        "name": "OBJECT_NAME",
+        "variable": "driver",
+        "variableTypes": ["driver"],
+        "defaultType": "driver"
+      },
+      {
+        "type": "field_dropdown",
+        "name": "METHOD",
+        "options": [
+          [
+            "ID名:",
+            "id"
+          ],
+          [
+            "name:",
+            "name"
+          ],
+          [
+            "クラス名:",
+            "class_name"
+          ],
+          [
+            "リンク名:",
+            "link_text"
+          ],
+          [
+            "一部のリンク名:",
+            "partial_link_text"
+          ],
+          [
+            "タグ名:",
+            "tag_name"
+          ],
+          [
+            "CSSセレクタ:",
+            "css_selector"
+          ],
+          [
+            "XPath:",
+            "xpath"
+          ]
+        ]
+      },
+      {
+        "type": "input_value",
+        "name": "NAME",
+        "check": "String"
+      }
+    ],
+    "inputsInline": true,
+    "output": [
+      "elements",
+      "Array"
+    ],
+    "colour": 180,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "driver_get_screenshot_as_xxx",
+    "message0": "%1 のスクリーンショットを %2 %3 で取得",
+    "args0": [
+      {
+        "type": "field_variable",
+        "name": "OBJECT_NAME",
+        "variable": "driver",
+        "variableTypes": ["driver"],
+        "defaultType": "driver"
+      },
+      {
+        "type": "field_dropdown",
+        "name": "METHOD",
+        "options": [
+          [
+            "png",
+            ".get_screenshot_as_png()"
+          ],
+          [
+            "base64",
+            ".get_screenshot_as_base64()"
+          ]
+        ]
+      },
+      {
+        "type": "input_dummy"
+      }
+    ],
+    "inputsInline": true,
+    "output": "String",
+    "colour": 180,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "driver_get_switch_to_active_element",
+    "message0": "%1 のアクティブな element を取得",
+    "args0": [
+      {
+        "type": "field_variable",
+        "name": "OBJECT_NAME",
+        "variable": "driver",
+        "variableTypes": ["driver"],
+        "defaultType": "driver"
+      }
+    ],
+    "inputsInline": true,
+    "output": "element",
+    "colour": 180,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "driver_get_switch_to_alert",
+    "message0": "%1 のアクティブなアラートを取得",
+    "args0": [
+      {
+        "type": "field_variable",
+        "name": "OBJECT_NAME",
+        "variable": "driver",
+        "variableTypes": ["driver"],
+        "defaultType": "driver"
+      }
+    ],
+    "inputsInline": true,
+    "output": "alert",
+    "colour": 180,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "driver_method",
+    "message0": "%1 で %2 を行う",
+    "args0": [
+      {
+        "type": "field_variable",
+        "name": "OBJECT_NAME",
+        "variable": "driver",
+        "variableTypes": ["driver"],
+        "defaultType": "driver"
+      },
+      {
+        "type": "field_dropdown",
+        "name": "METHOD",
+        "options": [
+          [
+            "戻る",
+            ".back()"
+          ],
+          [
+            "進む",
+            ".forward()"
+          ],
+          [
+            "更新",
+            ".refresh()"
+          ],
+          [
+            "最小化",
+            ".minimize_window()"
+          ],
+          [
+            "最大化",
+            ".maximize_window()"
+          ],
+          [
+            "全画面表示",
+            ".fullscreen_window()"
+          ],
+          [
+            "セッション開始",
+            ".start_session()"
+          ],
+          [
+            "閉じる",
+            ".close()"
+          ],
+          [
+            "終了",
+            ".quit()"
+          ]
+        ]
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 180,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "driver_method_add_cookie",
+    "message0": "%1 に名前: %2 値: %3 のクッキーを追加",
+    "args0": [
+      {
+        "type": "field_variable",
+        "name": "OBJECT_NAME",
+        "variable": "driver",
+        "variableTypes": ["driver"],
+        "defaultType": "driver"
+      },
+      {
+        "type": "input_value",
+        "name": "NAME",
+        "check": "String"
+      },
+      {
+        "type": "input_value",
+        "name": "VALUE",
+        "check": "String"
+      }
+    ],
+    "inputsInline": true,
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 180,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "driver_method_delete_cookie",
+    "message0": "%1 から名前: %2 のクッキーを削除",
+    "args0": [
+      {
+        "type": "field_variable",
+        "name": "OBJECT_NAME",
+        "variable": "driver",
+        "variableTypes": ["driver"],
+        "defaultType": "driver"
+      },
+      {
+        "type": "input_value",
+        "name": "NAME",
+        "check": "String"
+      }
+    ],
+    "inputsInline": true,
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 180,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "driver_method_delete_all_cookies",
+    "message0": "%1 の全てのクッキーを削除",
+    "args0": [
+      {
+        "type": "field_variable",
+        "name": "OBJECT_NAME",
+        "variable": "driver",
+        "variableTypes": ["driver"],
+        "defaultType": "driver"
+      }
+    ],
+    "inputsInline": true,
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 180,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "driver_method_execute_script",
+    "message0": "%1 でJavaScript: %2 %3 を実行",
+    "args0": [
+      {
+        "type": "field_variable",
+        "name": "OBJECT_NAME",
+        "variable": "driver",
+        "variableTypes": ["driver"],
+        "defaultType": "driver"
+      },
+      {
+        "type": "input_dummy"
+      },
+      {
+        "type": "input_value",
+        "name": "JAVASCRIPT",
+        "check": "String"
+      }
+    ],
+    "inputsInline": true,
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 180,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "driver_method_get",
+    "message0": "%1 でURL: %2 を開く",
+    "args0": [
+      {
+        "type": "field_variable",
+        "name": "OBJECT_NAME",
+        "variable": "driver",
+        "variableTypes": ["driver"],
+        "defaultType": "driver"
+      },
+      {
+        "type": "input_value",
+        "name": "NAME",
+        "check": "String"
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 180,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "driver_method_get_screenshot_as_file",
+    "message0": "%1 のスクリーンショットをファイル名: %2 %3 で保存",
+    "args0": [
+      {
+        "type": "field_variable",
+        "name": "OBJECT_NAME",
+        "variable": "driver",
+        "variableTypes": ["driver"],
+        "defaultType": "driver"
+      },
+      {
+        "type": "input_dummy"
+      },
+      {
+        "type": "input_value",
+        "name": "FILENAME",
+        "check": "String"
+      }
+    ],
+    "inputsInline": true,
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 180,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "driver_set_window_size",
+    "message0": "%1 のウィンドウサイズを %2 幅: %3 高さ: %4 に設定",
+    "args0": [
+      {
+        "type": "field_variable",
+        "name": "OBJECT_NAME",
+        "variable": "driver",
+        "variableTypes": ["driver"],
+        "defaultType": "driver"
+      },
+      {
+        "type": "input_dummy"
+      },
+      {
+        "type": "input_value",
+        "name": "WIDTH",
+        "check": "Number",
+        "align": "RIGHT"
+      },
+      {
+        "type": "input_value",
+        "name": "HEIGHT",
+        "check": "Number",
+        "align": "RIGHT"
+      }
+    ],
+    "inputsInline": true,
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 180,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "driver_set_window_position",
+    "message0": "%1 のウィンドウ位置を %2 縦: %3 横: %4 に設定",
+    "args0": [
+      {
+        "type": "field_variable",
+        "name": "OBJECT_NAME",
+        "variable": "driver",
+        "variableTypes": ["driver"],
+        "defaultType": "driver"
+      },
+      {
+        "type": "input_dummy"
+      },
+      {
+        "type": "input_value",
+        "name": "Y",
+        "check": "Number",
+        "align": "RIGHT"
+      },
+      {
+        "type": "input_value",
+        "name": "X",
+        "check": "Number",
+        "align": "RIGHT"
+      }
+    ],
+    "inputsInline": true,
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 180,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "driver_set_window_rect",
+    "message0": "%1 のウィンドウ位置とサイズを設定 %2 縦位置: %3 横位置: %4 幅: %5 高さ: %6",
+    "args0": [
+      {
+        "type": "field_variable",
+        "name": "OBJECT_NAME",
+        "variable": "driver",
+        "variableTypes": ["driver"],
+        "defaultType": "driver"
+      },
+      {
+        "type": "input_dummy"
+      },
+      {
+        "type": "input_value",
+        "name": "Y",
+        "check": "Number",
+        "align": "RIGHT"
+      },
+      {
+        "type": "input_value",
+        "name": "X",
+        "check": "Number",
+        "align": "RIGHT"
+      },
+      {
+        "type": "input_value",
+        "name": "WIDTH",
+        "check": "Number",
+        "align": "RIGHT"
+      },
+      {
+        "type": "input_value",
+        "name": "HEIGHT",
+        "check": "Number",
+        "align": "RIGHT"
+      }
+    ],
+    "inputsInline": false,
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 180,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "driver_set_implicitly_wait",
+    "message0": "%1 の要素が見つかるまでの待機時間を %2 %3 秒に設定",
+    "args0": [
+      {
+        "type": "field_variable",
+        "name": "OBJECT_NAME",
+        "variable": "driver",
+        "variableTypes": ["driver"],
+        "defaultType": "driver"
+      },
+      {
+        "type": "input_dummy"
+      },
+      {
+        "type": "input_value",
+        "name": "SEC",
+        "check": "Number"
+      }
+    ],
+    "inputsInline": true,
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 180,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "driver_set_page_load_timeout",
+    "message0": "%1 のページ読み込み完了の待機時間を %2 秒に設定",
+    "args0": [
+      {
+        "type": "field_variable",
+        "name": "OBJECT_NAME",
+        "variable": "driver",
+        "variableTypes": ["driver"],
+        "defaultType": "driver"
+      },
+      {
+        "type": "input_value",
+        "name": "TIMEOUT_SECOND",
+        "check": "Number"
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 180,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "driver_set_script_timeout",
+    "message0": "%1 のスクリプト待機時間を %2 秒に設定",
+    "args0": [
+      {
+        "type": "field_variable",
+        "name": "OBJECT_NAME",
+        "variable": "driver",
+        "variableTypes": ["driver"],
+        "defaultType": "driver"
+      },
+      {
+        "type": "input_value",
+        "name": "TIMEOUT_SECOND",
+        "check": "Number"
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 180,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "driver_method_switch_to_frame_window",
+    "message0": "%1 のフォーカスを変更 %2 指定方法： %3 %4",
+    "args0": [
+      {
+        "type": "field_variable",
+        "name": "OBJECT_NAME",
+        "variable": "driver",
+        "variableTypes": ["driver"],
+        "defaultType": "driver"
+      },
+      {
+        "type": "input_dummy"
+      },
+      {
+        "type": "field_dropdown",
+        "name": "METHOD",
+        "options": [
+          [
+            "フレーム",
+            "frame"
+          ],
+          [
+            "ウィンドウハンドル",
+            "window"
+          ]
+        ]
+      },
+      {
+        "type": "input_value",
+        "name": "NAME",
+        "check": [
+          "String",
+          "Number",
+          "element"
+        ],
+        "align": "RIGHT"
+      }
+    ],
+    "inputsInline": false,
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 180,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "driver_get_session_id",
+    "message0": "%1 のセッションIDを取得",
+    "args0": [
+      {
+        "type": "field_variable",
+        "name": "OBJECT_NAME",
+        "variable": "driver",
+        "variableTypes": ["driver"],
+        "defaultType": "driver"
+      }
+    ],
+    "inputsInline": true,
+    "output": "String",
+    "colour": 180,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "driver_get_command_executor_url",
+    "message0": "%1 の接続URLを取得",
+    "args0": [
+      {
+        "type": "field_variable",
+        "name": "OBJECT_NAME",
+        "variable": "driver",
+        "variableTypes": ["driver"],
+        "defaultType": "driver"
+      }
+    ],
+    "inputsInline": true,
+    "output": "String",
+    "colour": 180,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "driver_var_set_remote",
+    "lastDummyAlign0": "RIGHT",
+    "message0": "%1 にリモートのChromeドライバをセット %2 接続先URL(command_executor)： %3 ブラウザ表示： %4",
+    "args0": [
+      {
+        "type": "field_variable",
+        "name": "OBJECT_NAME",
+        "variable": "driver",
+        "variableTypes": ["driver"],
+        "defaultType": "driver"    
+      },
+      {
+        "type": "input_dummy"
+      },
+      {
+        "type": "input_value",
+        "name": "COMMAND_EXECUTOR",
+        "check": "String",
+        "align": "RIGHT"
+      },
+      {
+        "type": "field_dropdown",
+        "name": "OPTION_HEADLESS",
+        "options": [
+          [
+            "指定なし",
+            ""
+          ],
+          [
+            "表示しない",
+            "--headless"
+          ]
+        ]
+      }
+    ],
+    "inputsInline": false,
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 180,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "driver_set_session_id",
+    "message0": "%1 のセッションIDを %2 に設定",
+    "args0": [
+      {
+        "type": "field_variable",
+        "name": "OBJECT_NAME",
+        "variable": "driver",
+        "variableTypes": ["driver"],
+        "defaultType": "driver"    
+      },
+      {
+        "type": "input_value",
+        "name": "SESSHION_ID"
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 180,
+    "tooltip": "",
+    "helpUrl": ""
+  }]
+  );
